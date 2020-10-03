b478f6b2edba18ad0e39c62af44239a2f079773f
JunoNgx <juno.ngx@gmail.com>
2020-10-03 10:42:34 +0800

    Implement mock result page

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ function About() {
 
     return (
         <div className="about">
-            <p>This application was created as a data collection tool for a research project as part of the study programme under Murdoch University. Data are self-contained within the partipant's browser and handed over from explicitly from partipants to the researchers via screenshots or other forms.</p>
+            <p>This application was used as a data collection tool for a research project as part of the study programme under Murdoch University (Australia). Data are self-contained within the partipant's browser and handed over from explicitly from partipants to the researchers via screenshots or other forms.</p>
             <p>Created by <a href="https://junongx.com/" rel="noopener" target="_blank">Juno Nguyen</a> with <a href="https://reactjs.org/" rel="noopener" target="_blank">ReactJS</a> and <a href="hhttps://sass-lang.com/" rel="noopener" target="_blank">SASS</a>. The source code is open and distributed under MIT License:</p>
             <p><a href="https://github.com/JunoNgx/working-memory-test" rel="noopener" target="_blank">https://github.com/JunoNgx/working-memory-test</a></p>
             <p>For any question or issue, please contact Juno via appropriate channels.</p>
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 function Results() {
+
+    const history = useHistory()
+
     return (
-        <div>
+        <div className="results">
+            <p className="results__title">Test results so far:</p>
             
+            <div className="results__container">
+                <p className="results__container__test-name">N-back</p>
+                <p className="results__container__results">4 4 2 1 0</p>
+            </div>
+            
+            <div className="results__container">
+                <p className="results__container__test-name">Memory updating</p>
+                <p className="results__container__results">0 1 3 4 5</p>
+            </div>
+            
+            <div className="results__container">
+                <p className="results__container__test-name">Corsi-block tapping</p>
+                <p className="results__container__results">5 4 3 2 1</p>
+            </div>
+
+            <p className="results__send-request">You have completed your tests. Please take a screenshot of this page and send to your point of contact in the research team. Your co-operation is much appreciated.</p>
+
+            <button className="button--back" onClick={()=>{history.push("/menu")}}>Back</button>
         </div>
     )
 }
