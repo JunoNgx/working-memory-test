61d2be7a5b4f6e19460697983f336ae235ad8c6f
JunoNgx <juno.ngx@gmail.com>
2020-10-03 13:39:25 +0800

    Update notice content

--- src/pages/Agreement.js
+++ src/pages/Agreement.js
@@ -6,7 +6,7 @@ function Agreement() {
 
     return (
         <div className="agreement">
-            <p>This application was used as a data collection tool for a research project as part of the study programme under Murdoch University (Australia). Data are self-contained within the partipant's browser and handed over from explicitly from partipants to the researchers via screenshots or other forms.</p>
+            <p>This application was used as a data collection tool for a research project as part of the study programme under Murdoch University (Australia). Data are self-contained within the partipant's browser and handed over explicitly from the participant to the research team via screenshots or other forms of communications.</p>
             <p>For data accuracy and validity of the research, please refrain from using assistive method or device, such as recording and/or taking notes, during the course of the tests.</p>
             <p>By clicking the button below, you accept to use the app with our terms and relinquish us of any legal obligation.</p>
 
